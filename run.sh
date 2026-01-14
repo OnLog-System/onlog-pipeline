a9d8ca6b120e965271a0115bc19126c4586a6a48
Yoonseok Kang <kangcl1609@gmail.com>
2026-01-14 20:26:29 +0900

    Refactor run script to set AWS_REGION directly and remove unnecessary AWS credentials environment variables

--- run.sh
+++ run.sh
@@ -17,12 +17,11 @@ echo "[3/4] Run container"
 docker run -d \
   --name onlog-msk-producer \
   --restart unless-stopped \
-  -e AWS_ACCESS_KEY_ID \
-  -e AWS_SECRET_ACCESS_KEY \
-  -e AWS_REGION \
+  -e AWS_REGION=ap-northeast-2 \
   -e KAFKA_BOOTSTRAP_SERVERS \
   -e DB_BASE_PATH \
   -e TOPIC_PREFIX \
+  -v /home/ubuntu/.aws:/root/.aws:ro \
   -v "$DB_BASE_PATH:$DB_BASE_PATH:ro" \
   onlog/msk-producer:latest
 
